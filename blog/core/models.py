4ad5c09a089ba5240b587353eb6dbf7fa155fd1e
Vlad D <w.demenkow@gmail.com>
2024-02-18 18:37:07 +0100

    [fixed]: creatusuperuser without username

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,8 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
+    username = None
     email = models.EmailField(verbose_name='email address',
                               max_length=255,
                               unique=True)
@@ -19,13 +54,17 @@ class CustomUser(AbstractUser):
         null=True
     )
 
-    REQUIRED_FIELDS = ['email', 'phone_number']
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['phone_number']
+
+    objects = CustomUserManager()
 
     class Meta:
         ordering = ['last_name', 'first_name']
 
     def __str__(self):
-        return self.email
+        return f"{self.last_name}, Admin: {self.is_admin}"
 
 
 # class Record(models.Model):
